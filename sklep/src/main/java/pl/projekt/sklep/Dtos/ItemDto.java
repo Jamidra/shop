0a49ea960141bbac3ec61e4cc4f27d3bfc6b54ca
Arnamis <bartoszck@gmail.com>
2025-05-14 22:30:23 +0200

    Add files via upload

--- sklep/src/main/java/pl/projekt/sklep/Dtos/ItemDto.java
+++ sklep/src/main/java/pl/projekt/sklep/Dtos/ItemDto.java
@@ -13,4 +13,66 @@ public class ItemDto {
     private int inventory;
     private String description;
     private Category category;
+
+    public ItemDto(Long id, String name, String brand, BigDecimal price, int inventory, String description, Category category) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 }
--- sklep/src/main/java/pl/projekt/sklep/Dtos/UserDto.java
+++ sklep/src/main/java/pl/projekt/sklep/Dtos/UserDto.java
@@ -7,4 +7,43 @@ public class UserDto {
     private String firstName;
     private String lastName;
     private CartDto cart;
+
+    public UserDto(Long id, String firstName, String lastName, CartDto cart) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cart = cart;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public CartDto getCart() {
+        return cart;
+    }
+
+    public void setCart(CartDto cart) {
+        this.cart = cart;
+    }
 }
